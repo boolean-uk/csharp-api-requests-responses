5c2ae60f6c18f6e8068f3702b4a147eae9e47aef
konas <konas@LAPTOP-1GHMR8NV>
2023-09-26 18:04:31 +0300

    finished core requirements

--- request-response/request-response.Main/Controllers/LanguageControlles.cs
+++ request-response/request-response.Main/Controllers/LanguageControlles.cs
@@ -37,5 +37,48 @@ namespace request_response.Controllers
         {
             return Results.Ok(_languages);
         }
+
+        // get a language
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("{name}")]
+        public async Task<IResult> GetALanguage(string name)
+        {
+            var language = _languages.FirstOrDefault(lang => lang.Name == name);
+            return language != null ? Results.Ok(language) : Results.NotFound();
+        }
+
+        // update a language
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("{name}")]
+        public async Task<IResult> UpdateALanguage(string name, Language newLanguage)
+        {
+            var language = _languages.FirstOrDefault(lang => lang.Name == name);
+            if (language == null) return Results.NotFound();
+
+            if (!string.IsNullOrEmpty(newLanguage.Name))
+            {
+                language.Name = newLanguage.Name;
+            }
+
+            return Results.Created($"http://localhost:5186/Language/{language.Name}", language);
+        }
+
+        // delete a language
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("{name}")]
+        public async Task<IResult> DeleteALanguage(string name)
+        {
+            var language = _languages.FirstOrDefault(lang => lang.Name == name);
+            bool result = _languages.Remove(language);
+
+            if (!result || language == null) return Results.NotFound();
+            return Results.Ok(language);
+        }
     }
 }
