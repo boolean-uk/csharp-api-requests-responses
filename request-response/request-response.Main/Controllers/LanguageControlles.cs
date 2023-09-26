51e9242b75ac41c3327a904eb4a70f1a9a874369
konas <konas@LAPTOP-1GHMR8NV>
2023-09-26 17:50:23 +0300

    create a language

--- request-response/request-response.Main/Controllers/LanguageControlles.cs
+++ request-response/request-response.Main/Controllers/LanguageControlles.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using request_response.Models;
 
 namespace request_response.Controllers
 {
@@ -7,8 +8,26 @@ namespace request_response.Controllers
     [Route("[controller]")]
     public class LanguageController : ControllerBase
     {
+        private static List<Language> _languages = new List<Language>();
+
         public LanguageController()
         {
+            if (_languages.Count == 0)
+            {
+                _languages.Add(new Language("C#"));
+                _languages.Add(new Language("Java"));
+            }
         }
+
+        // create a language
+        [HttpPost]
+        [Route("")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IResult> CreateALanguage(Language language)
+        {
+            _languages.Add(language);
+            return Results.Created($"http://localhost:5186/Language/{language.Name}", language);
+        }
+
     }
 }
