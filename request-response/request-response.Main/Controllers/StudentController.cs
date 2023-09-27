542995276813b959e142afd9e33eb6a9e58020d3
konas <konas@LAPTOP-1GHMR8NV>
2023-09-27 08:02:40 +0300

    get all students

--- request-response/request-response.Main/Controllers/StudentController.cs
+++ request-response/request-response.Main/Controllers/StudentController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using request_response.Models;
 
 namespace request_response.Controllers
 {
@@ -7,8 +8,27 @@ namespace request_response.Controllers
     [Route("[controller]")]
     public class StudentController : ControllerBase
     {
+        private static List<Student> _students = new List<Student>();
+
         public StudentController()
         {
+            if (_students.Count == 0)
+            {
+                _students.Add(new Student("Nathan", "King"));
+                _students.Add(new Student("Nigel", "Sibbert"));
+                _students.Add(new Student("Dave", "Ames"));
+            }
+        }
+
+        // TODO: create a student
+
+
+        // get all students
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IResult> GetStudents()
+        {
+            return Results.Ok(_students);
         }
     }
 }
