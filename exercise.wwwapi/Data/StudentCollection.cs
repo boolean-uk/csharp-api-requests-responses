ebd5692bcbf2927c00e3568b9e49cf1708d6a2b6
Malte <malte.stengard@gmail.com>
2024-08-29 13:30:23 +0200

    All student functionality working

--- exercise.wwwapi/Data/StudentCollection.cs
+++ exercise.wwwapi/Data/StudentCollection.cs
@@ -6,8 +6,8 @@ namespace exercise.wwwapi.Data
     {
         private static List<Student> _students = new List<Student>()
         {
-            new Student() { FirstName="Nathan",LastName="King" },
-            new Student() { FirstName="Dave", LastName="Ames" }
+            new Student() { Id = 1, FirstName="Nathan",LastName="King" },
+            new Student() { Id = 2, FirstName="Dave", LastName="Ames" }
         };
 
         public static Student Add(Student student)
--- exercise.wwwapi/Endpoints/StudentController.cs
+++ exercise.wwwapi/Endpoints/StudentController.cs
@@ -6,27 +6,59 @@ namespace exercise.wwwapi.Endpoints
 {
     [ApiController]
     [Route("students")]
-    public static class StudentController 
+    public static class StudentController
     {
         public static void ConfigureStudentController(this WebApplication app)
         {
             var students = app.MapGroup("students");
             students.MapGet("/", GetStudents);
             students.MapPost("/", AddStudent);
-            //students.MapGet("/", GetStudents);
-            //students.MapGet("/", GetStudents);
-            //students.MapGet("/", GetStudents);
+            students.MapGet("/{id}", GetStudent);
+            students.MapPut("/", UpdateStudent);
+            students.MapDelete("/", DeleteStudent);
         }
 
-        [HttpPost]
-        [Route("")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        private static IResult DeleteStudent(IStudentRepository repository, int id)
+        {
+            return TypedResults.Ok(repository.DeleteStudent(id));
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        private static IResult UpdateStudent(IStudentRepository repository, Student student)
+        {
+            return TypedResults.Ok(repository.UpdateStudent(student));
+        }
+
+        //[HttpGet]
+        //[]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public static IResult GetStudent(IStudentRepository repository, int id)
+        {
+            return TypedResults.Ok(repository.GetStudent(id));
+        }
+
+        //[HttpPost]
+        //[Route("")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public static IResult AddStudent(IStudentRepository repository, Student student)
         {
             return TypedResults.Ok(repository.AddStudent(student));
         }
 
-        [HttpGet]
-        [Route("")]
+        //[HttpGet]
+        //[Route("")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public static IResult GetStudents(IStudentRepository repository)
         {
             return TypedResults.Ok(repository.GetStudents());
