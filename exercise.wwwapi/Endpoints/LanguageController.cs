f592548fd2b1891471e482fbe06ed60c0b810e81
Malte <malte.stengard@gmail.com>
2024-08-29 13:43:34 +0200

    Language controller populated

--- exercise.wwwapi/Endpoints/LanguageController.cs
+++ exercise.wwwapi/Endpoints/LanguageController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using exercise.wwwapi.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace exercise.wwwapi.Endpoints
 {
@@ -9,6 +10,52 @@ namespace exercise.wwwapi.Endpoints
         public static void ConfigureLanguageController(this WebApplication app)
         {
             var languages = app.MapGroup("languages");
+            languages.MapGet("/{id}", GetLanguage);
+            languages.MapGet("/", GetLanguages);
+            languages.MapPost("/", AddLanguage);
+            languages.MapPut("/", UpdateLanguage);
+            languages.MapDelete("/", DeleteLanguage);
+
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        private static IResult DeleteLanguage(ILanguageRepository repository, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        private static IResult UpdateLanguage(ILanguageRepository repository, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        private static IResult AddLanguage(ILanguageRepository repository, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        private static IResult GetLanguages(ILanguageRepository repository)
+        {
+            return TypedResults.Ok(repository.GetLanguages());
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        private static IResult GetLanguage(ILanguageRepository repository, string name)
+        {
+            return TypedResults.Ok(repository.GetLanguage(name));
 
         }
     }
