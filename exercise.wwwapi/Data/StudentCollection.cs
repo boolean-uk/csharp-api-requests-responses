031b735fbb5ab56294af3f0e0a98975adeccdf30
Ali Haider Khan <ali_786@live.no>
2024-08-30 12:03:01 +0200

    core done.

--- exercise.wwwapi/Data/StudentCollection.cs
+++ exercise.wwwapi/Data/StudentCollection.cs
@@ -2,26 +2,48 @@
 
 namespace exercise.wwwapi.Data
 {
-    public class StudentCollection
+    public static class StudentCollection
     {
-        private List<Student> _students = new List<Student>()
+        private static List<Student> _students = new List<Student>()
         {
             new Student() { FirstName="Nathan",LastName="King" },
             new Student() { FirstName="Dave", LastName="Ames" }
         };
 
-        public Student Add(Student student)
-        {            
+        public static List<Student> Students { get { return _students; } }
+
+        public static Student Create(Student student)
+        {
             _students.Add(student);
 
             return student;
         }
 
-        public List<Student> getAll()
+        public static List<Student> getAll()
         {
             return _students.ToList();
         }
-    };
+        public static Student GetStudent(string firstname)
+        {
+            return _students.Find(x => x.FirstName == firstname);
+        }
+        public static Student UpdateStudent(string firstname, Student student)
+        {
+            int index = _students.FindIndex(student => student.FirstName == firstname);
+
+            _students[index] = student;
+
+            return student;
+        }
+        public static Student DeleteStudent(string firstname)
+        {
+
+            var deletedstudent = _students.FirstOrDefault(x => x.FirstName == firstname);
+            _students.Remove(deletedstudent);
+            return deletedstudent;
+
+        }
+    }
 
 
 }
