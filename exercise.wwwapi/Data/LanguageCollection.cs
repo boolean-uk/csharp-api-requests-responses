6531d25e6ff3949935a0a30dfc4af4275decfc51
stp <steventphung6@gmail.com>
2025-01-17 13:34:10 +0100

    Steven Phung

--- exercise.wwwapi/Data/LanguageCollection.cs
+++ exercise.wwwapi/Data/LanguageCollection.cs
@@ -5,9 +5,41 @@ namespace exercise.wwwapi.Data
 
     public class LanguageCollection
     {
-        private List<Language> languages = new List<Language>(){
-            new Language("Java"),
-            new Language("C#")
-        };
+        private static List<Language> _language = new List<Language>();
+
+        public static void Initialize()
+        {
+            _language.Add(new Language { Name = "Java" });
+            _language.Add(new Language { Name = "Csharp" });
+        }
+        public static List<Language> Languages { get { return _language; } }
+
+        public static Language Get(string Name)
+        {
+            return _language.FirstOrDefault(l => l.Name == Name);
+        }
+        public static Language Add(Language language)
+        {
+            var existingLanguage = _language.FirstOrDefault(l => l.Name == l.Name);
+            if (existingLanguage != null)
+            {
+                _language.Add(language);
+            }
+            return language;
+        }
+        public static Language Update(string name, Language language)
+        {
+            var existingLanguage = _language.FirstOrDefault(l => l.Name == name);
+            if (existingLanguage != null)
+            {
+                existingLanguage.Name = language.Name;
+                
+            }
+            return existingLanguage;
+        }
+        public static bool Delete(string name)
+        {
+            return _language.RemoveAll(l => l.Name == name) > 0 ? true : false;
+        }
     }
 }
---
+++ exercise.wwwapi/Repository/StudentRepository.cs
@@ -0,0 +1,34 @@
+﻿using exercise.wwwapi.Data;
+using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.Repository
+{
+    public class StudentRepository : IRepository
+    {        
+        public Student GetStudent(string firstName)
+        {
+            return StudentCollection.Get(firstName);
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return StudentCollection.Students;
+        }
+
+        public Student AddStudent(Student student)
+        {
+            return StudentCollection.Add(student);
+            
+        }
+
+        public bool DeleteStudent(string firstName)
+        {
+            return StudentCollection.DeleteStudent(firstName);
+        }
+
+        public Student UpdateStudent(string firstName, Student student)
+        {
+            return StudentCollection.Update(firstName, student);
+        }
+    }
+}
