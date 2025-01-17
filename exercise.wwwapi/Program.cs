e8fd2d8f6d4c68b1b7b6cfe284c199cedad34914
Hans Eivind Johnsen Skinstad <Hans.Eivind.Skinstad@no.experis.com>
2025-01-17 12:48:44 +0100

    Tried to implement generic repository. Commit before trying to revert some of this...

--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -1,3 +1,5 @@
+using exercise.wwwapi.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
---
+++ exercise.wwwapi/Repository/StudentRepository.cs
@@ -0,0 +1,35 @@
+﻿using exercise.wwwapi.Data;
+using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.Repository
+{
+    public class StudentRepository<T> : IRepository<T>
+    {
+        public T Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IEnumerable<StudentCollection> students = StudentCollection.Students;
+
+            return students;
+        }
+
+        public T Update(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
